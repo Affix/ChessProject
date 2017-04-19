93806e8de6bd3f575be345e4dff29d60faf3f776
Keiran Smith <affix@affix.me>
2017-04-19 15:40:15 +0100

    Implement Rook Movement Validator

--- ChessProject-PHP/src/Pieces/MovementValidator/RookMovementValidator.php
+++ ChessProject-PHP/src/Pieces/MovementValidator/RookMovementValidator.php
@@ -7,8 +7,52 @@ use LogicNow\ChessBoard;
 
 class RookMovementValidator implements MovementValidator
 {
+  protected $_piece;
+
   public function validate(Piece $piece, $x, $y)
   {
+    $old_x = $piece->getXCoordinate();
+    $old_y = $piece->getYCoordinate();
+    $this->_piece = $piece;
+    return abs($old_x - $x) <= $this->_piece::MOVE_LIMIT &&
+       abs($old_y - $y) <= $this->_piece::MOVE_LIMIT &&
+       $this->isLegalPath($x, $y);
+  }
 
+  private function isDiagonalMove($newX, $newY)
+  {
+    return $newX != $this->_piece->getXCoordinate() &&
+           $newY != $this->_piece->getYCoordinate();
+  }
+
+  private function isLegalPath($newX, $newY)
+  {
+    $_chessBoard = $this->_piece->getChesssBoard();
+    $_oldX = $this->_piece->getXCoordinate();
+    $_oldY = $this->_piece->getYCoordinate();
+    if(!$this->isDiagonalMove($newX, $newY))
+    {
+      if($newX != $_oldX)
+      {
+        for($i = ($_oldX + 1); $i <= $newX; $i++)
+        {
+          if(!$_chessBoard->isLegalBoardPosition($i, $newY))
+          {
+            return false;
+          }
+        }
+        return true;
+      }
+      else {
+        for($i = ($_oldY + 1); $i <= $newY; $i++)
+        {
+          if(!$_chessBoard->isLegalBoardPosition($newX, $i))
+          {
+            return false;
+          }
+        }
+        return true;
+      }
+    }
   }
 }
--- ChessProject-PHP/src/Pieces/Rook.php
+++ ChessProject-PHP/src/Pieces/Rook.php
@@ -22,54 +22,10 @@ class Rook extends Piece
 
     public function move(MovementTypeEnum $movementTypeEnum, $newX, $newY)
     {
-      $old_x = $this->getXCoordinate();
-      $old_y = $this->getYCoordinate();
-
-      if(abs($old_x - $newX) <= self::MOVE_LIMIT &&
-         abs($old_y - $newY) <= self::MOVE_LIMIT &&
-         $this->isLegalPath($newX, $newY)
-       )
+      if($this->_validator->validate($this, $newX, $newY))
       {
         $this->setYCoordinate($newY);
         $this->setXCoordinate($newX);
       }
     }
-
-    private function isDiagonalMove($newX, $newY)
-    {
-      return $newX != $this->getXCoordinate() &&
-             $newY != $this->getYCoordinate();
-    }
-
-    private function isLegalPath($newX, $newY)
-    {
-      $_chessBoard = $this->getChesssBoard();
-      $_oldX = $this->getXCoordinate();
-      $_oldY = $this->getYCoordinate();
-      if(!$this->isDiagonalMove($newX, $newY))
-      {
-        if($newX != $_oldX)
-        {
-          for($i = ($_oldX + 1); $i <= $newX; $i++)
-          {
-            if(!$_chessBoard->isLegalBoardPosition($i, $newY))
-            {
-              return false;
-            }
-          }
-          return true;
-        }
-        else {
-          for($i = ($_oldY + 1); $i <= $newY; $i++)
-          {
-            if(!$_chessBoard->isLegalBoardPosition($newX, $i))
-            {
-              return false;
-            }
-          }
-          return true;
-        }
-      }
-    }
-
 }
